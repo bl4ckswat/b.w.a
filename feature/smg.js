fedae60e54b71bc5922b576f3398ad17051e1f73
bl4ckswat <122009260+bl4ckswat@users.noreply.github.com>
2023-01-08 11:42:52 +0700

    Update smg.js

--- feature/smg.js
+++ feature/smg.js
@@ -3,14 +3,18 @@ const { Client, MessageMedia } = require('whatsapp-web.js');
 const SmgHandler = async (text, msg, client) => {
 
     msg.reply('  ∧,,,∧\n(   ̳• · • ̳)\n/    づ Prakiraan cuaca Semarang 🤔⛅');
+    try {
+        const chat = await msg.getChat();
+        const media = await MessageMedia.fromUrl('http://cuacajateng.com/prakiraan/image/img_terkini/semarang.png');
+        chat.sendMessage(media, { caption: '*Terkini* ✨' });
 
-    const chat = await msg.getChat();
-    const media = await MessageMedia.fromUrl('http://cuacajateng.com/prakiraan/image/img_terkini/semarang.png');
-    chat.sendMessage(media, { caption: '*Terkini* ✨' });
+        const media2 = await MessageMedia.fromUrl('http://cuacajateng.com/prakiraan/image/img_besok/semarang.png');
+        chat.sendMessage(media2, { caption: 'Besok' })
+        console.log(media.filename, media2.filename, 'Success!');
+    } catch (error) {
+        return msg.reply(`Terjadi kesalahan 😵\n\n*${error}*\n\ncoba lagi nanti atau gunakan fitur lain *?help*`);
+    }
 
-    const media2 = await MessageMedia.fromUrl('http://cuacajateng.com/prakiraan/image/img_besok/semarang.png');
-    chat.sendMessage(media2, { caption: 'Besok' })
-    console.log(media.filename, media2.filename, 'Success!');
 };
 
 module.exports = {
